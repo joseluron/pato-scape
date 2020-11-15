cb9501ccab7604d6dcfa4bd023c09ff6e8efcdb5
luron <joseluron@gmail.com>
2020-11-15 21:28:08 +0100

    Made some changes to code format
     - Also, installed react-router-dom and node-sass

---
+++ src/pages/Main/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainPage = () => {
+  return (
+    <div className="main-page-container">
+      <span>Main page</span>
+    </div>
+  );
+}
+
+export default MainPage;
